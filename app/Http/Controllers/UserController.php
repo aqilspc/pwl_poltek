42e7e5484b873f5e9fb858a4079eb24961414054
root <aqil@intermediatech.id>
2021-09-28 03:09:39 +0700

    up api video

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,18 +7,45 @@ use DB;
 use Carbon\Carbon;
 use Excel;
 use App\Exports\UserExport;
+use Google\Cloud\Storage\StorageClient;
 class UserController extends Controller
 {
 
     public function getMemberUser()
     {
         $date = rand();
-
         Excel::store(new UserExport(), 'user_member_download_at_'.$date.'_.xlsx','real_public');
         return response()->json(
               [
                 'msg' => 'Sucess',
                 'url'=>url('/').'/'.'user_member_download_at_'.$date.'_.xlsx',
+              ], 201);
+    }
+
+    public function upVideo(Request $request)
+    {
+        $ext = 'mp4';
+        $config = [
+          'projectId' => 'optical-net-251109',
+          'keyFile' => json_decode(file_get_contents('https://dashboard.kulon.co.id/upload/key.json'), true),
+        ];
+
+        //request
+        $tmp = $request->file('video')->getPathName();
+        $id = $request->id;
+
+        $storage = new StorageClient($config);
+        $pho = 'img_'.$id.'.'.$ext;
+        $file = fopen($tmp, 'r');
+        $bucket = $storage->bucket('admin-panel-kulon');
+        $object = $bucket->upload($file, [
+          'name' => 'video/'.$pho
+        ]);
+
+        return response()->json(
+              [
+                'msg' => 'Sucess',
+                'data'=>$pho,
               ], 201);
     }
 }
