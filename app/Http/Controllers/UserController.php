ff9ee68f2e774c68620b2f8c7ec7ca04f91034af
root <aqil@intermediatech.id>
2021-09-28 03:51:21 +0700

    ll

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,6 +24,11 @@ class UserController extends Controller
 
     public function upVideo(Request $request)
     {
+      header("Access-Control-Allow-Origin: *");
+      header("Access-Control-Allow-Credentials: true ");
+      header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+      header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+      
         $ext = 'mp4';
         $config = [
           'projectId' => 'optical-net-251109',
